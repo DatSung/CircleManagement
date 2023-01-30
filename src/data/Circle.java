5e9a686e7aaa2a3c850161c002e37e53c87fc2ff
Nguyen Thanh Dat <109453487+DatSung@users.noreply.github.com>
2023-01-30 10:02:39 +0700

    comint

--- src/data/Circle.java
+++ src/data/Circle.java
@@ -10,18 +10,28 @@ import java.util.*;
  * @author uinic
  */
 public class Circle {
+//    ATTRIBUTES
     private double x;
     private double y;
     private double r;
 
-    public void inputCircle() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Input x: ");
-        x = input.nextDouble();
-        System.out.println("Input y: ");
-        y = input.nextDouble();
-        System.out.println("Input r: ");
-        r = input.nextDouble();
+
+//SETTER AND GETTER
+
+
+    public void setX(double x) {
+        if (x > 0)
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        if (y > 0)
+        this.y = y;
+    }
+
+    public void setR(double r) {
+        if (r > 0)
+        this.r = r;
     }
 
     public double getX() {
@@ -38,6 +48,28 @@ public class Circle {
 
     public void outputCircle() {
         System.out.printf("|%4.1f|%4.1f|%4.1f|\n", x, y, r);
+    }
+//CONSTRUCTOR
+    public Circle() {
+        x = 0;
+        y = 0;
+        r = 0;
+    }
+
+    public Circle(double x, double y, double r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+
+    public void inputCircle() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input x: ");
+        x = input.nextDouble();
+        System.out.println("Input y: ");
+        y = input.nextDouble();
+        System.out.println("Input r: ");
+        r = input.nextDouble();
     }
 
     public double getDistance(double x2, double y2) {
